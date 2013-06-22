9dfabc29dcae53f6e704dde6c33c9370f64b7a1a
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-06-22 19:27:29 +0200

    Back to development: 2.0.67

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup, find_packages
 
 name = 'minitage'
-version = '2.0.66'
+version = '2.0.67.dev0'
 def read(rnames):
     setupdir =  os.path.dirname( os.path.abspath(__file__))
     return open(
